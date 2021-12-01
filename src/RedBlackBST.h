8e861c3c2bb6f83cee50250ed0360f8442cc8eef
Ryan Blazejewski <47003070+rblazej1@users.noreply.github.com>
2021-12-01 16:17:44 -0500

    RedBlack remove function
    not tested yet, will do so in a bit

--- src/RedBlackBST.h
+++ src/RedBlackBST.h
@@ -140,6 +140,98 @@ class RedBlackBST : public BST<T> {
     }
     return node;
   }
+ 
+ void remove(Node<T>* node) {
+  //BST delete and get replacement node
+  Node<T>* replace = BST<T>::remove(node);
+  
+  //Cannot have double red by property of red-black tree
+  assert(!(node->color == RED && replace->color == RED))
+  
+  Node<T>* sibling = getSibling(node);
+  //node is a leaf
+  if(replace == nullptr)
+  {
+    if(node->color == BLACK)
+      doubleBlack(node);
+    else {
+      if(sibling)
+        sibling->color = RED; 
+    }
+    return;
+  }
+  
+  //Simple case of one red
+  if(node->color == RED || replace->color == RED) {
+    replace->color == BLACK;
+    return;
+  }
+  else {
+   doubleBlack(replace);
+   return;
+  }
+ }
+ void doubleBlack(Node<T>* node) {
+   //no need to fix if node is root
+   if(node == root)
+    return;
+   
+   Node<T>* parent = getParent(node);
+   Node<T>* sibling = getSibling(node);
+ 
+   if(sibling) {
+     bool redRight = (sibling->right != nullptr && sibling->right->color == RED);
+     bool redLeft = (sibling->left != nullptr && sibling->left->color == RED);
+     bool leftLeft = (redLeft && sibling == sibling->parent->left);
+     bool rightLeft = (redLeft && !(sibling == sibling->parent->left));
+     bool rightRight = (redRight && !(sibling == sibling->parent->left));
+     bool leftRight = (redRight && sibling == sibling->parent->left);
+     //cases 3.2a1 - 3.2b on geeksforgeeks https://www.geeksforgeeks.org/red-black-tree-set-3-delete-2/
+     if(sibling->color == BLACK) {
+       if(leftLeft) {
+         sibling->left->color = sibling->color;
+         sibling->color = parent->color;
+         rightRotate(parent);
+         parent->color = BLACK;
+       }
+       else if(rightLeft) {
+         sibling->left->color = parent->color;
+         rightRotate(sibling);
+         leftRotate(parent);
+         parent->color = BLACK;
+       }
+       else if(leftRight) {
+         sibling->right->color = parent->color;
+         leftRotate(sibling);
+         rightRotate(parent);
+         parent->color = BLACK;
+       }
+       else if(rightRight) {
+         sibling->right->color = sibling->color;
+         sibling->color = parent->color;
+         leftRotate(parent);
+         parent->color = BLACK;
+       }
+       else {
+         sibling->color = RED;
+         if(parent->color == BLACK)
+           doubleBlack(parent);
+         else
+           parent->color = BLACK;
+       }
+     }
+     //case 3.3 on geeksforgeeks https://www.geeksforgeeks.org/red-black-tree-set-3-delete-2/
+     else {
+       parent->color = RED;
+       sibling->color = BLACK;
+       if(sibling == sibling->parent->right)
+         leftRotate(parent);
+       else
+         rightRotate(parent);
+      
+       doubleBlack(node);
+     }
+     return;
 };
 
 #endif
