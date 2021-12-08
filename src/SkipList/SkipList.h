241ee957166152d0621033c3737fb32462d48e95
Alan Bixby <abixby1@binghamton.edu>
2021-12-08 09:28:56 -0500

    Added getBuildString to skipList

--- src/SkipList/SkipList.h
+++ src/SkipList/SkipList.h
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #include "Map.h"
+#include "../helpers/pdebug.h"
 
 using namespace std;
 
@@ -11,11 +12,13 @@ template <class T>
 struct SkipList : public Map<string, string> {
   SkipList<T>() : Map<string, string>(){};
   ~SkipList<T>(){};
+
   void insert(string value) {
     Map<string, string>::insert({{value}, {value}});
   };
+
   int getHeight() { return currLevel; };
-  string getBuildString() { return "[{\"disabled\": \"true\"}]"; };
+
   bool find(string value) {
     try {
       auto item = *Map<string, string>::find(value);
@@ -24,6 +27,7 @@ struct SkipList : public Map<string, string> {
       return false;
     }
   };
+
   void remove(string value) {
     try {
       Map<string, string>::erase(value);
@@ -31,13 +35,16 @@ struct SkipList : public Map<string, string> {
       cout << "";
     }
   };
+
   void print() { levelByLevel(); };
+
   void inorder() {
     for (auto it = begin(); it != end(); ++it) {
       cout << std::get<0>(*(it)) << " ";
     }
     cout << endl;
   };
+
   void levelByLevel() {
     for (int i = 0; i <= currLevel; i++) {
       SLNode *node = head->next[i];
@@ -50,6 +57,31 @@ struct SkipList : public Map<string, string> {
       }
       cout << endl;
     }
+  }
+
+  string getBuildString([[maybe_unused]] bool color = false) {
+    stringstream ss;
+    for (int i = 0; i <= currLevel; i++) {
+      SLNode *node = head->next[i];
+      stringstream temp;
+      temp << "{ \"level\": " << i << ", \"set\": [";
+      bool isEmpty = true;
+      while (node != nullptr) {
+        if (node->key != nullptr) {
+          temp << "\"" << std::get<0>(*(node->key)) << "\", ";
+          isEmpty = false;
+        }
+        node = node->next[i];
+      }
+      string levelJson = temp.str();
+      levelJson = levelJson.substr(0, levelJson.length() - 2) + (isEmpty ? " [" : "") + "] },\n";
+      ss << levelJson;
+    }
+
+    string output = ss.str();
+    output = "[" + output.substr(0, output.length() - 0) + "{ \"style\": \"SkipList\" }]";
+    pdebug_val(output);
+    return output;
   }
 };
 
